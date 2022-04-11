0b1456344d7ea86d733333c34a28969824951930
A1doranM <vaselisk1997@gmail.com>
2022-04-11 13:08:19 +0300

    create header scratch

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/me.png";
 
 function Header(props) {
     return (
-        <div></div>
+        <header>
+            <div className="container header__container">
+                <h5>Hello I`m</h5>
+                <h1>Maksym Medvediev</h1>
+                <h5 className="text-light">Fullstack Developer</h5>
+                <CTA/>
+
+                <div className="me">
+                    <img src={ME} alt="Image of me."/>
+                </div>
+
+                <a href="#contacts" className="scroll_down">Scroll Down</a>
+            </div>
+        </header>
     );
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HeaderSocials(props) {
+    return (
+        <div className="header__socials">
+            <a href="https://linkedin.com" target="_blank"></a>
+            <a href="https://linkedin.com" target="_blank"></a>
+        </div>
+    );
+}
+
+export default HeaderSocials;
