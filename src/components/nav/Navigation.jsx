a0de60877f30239fd6a4de70b07bc3a5654e9301
A1doranM <vaselisk1997@gmail.com>
2022-04-11 15:11:49 +0300

    completed navigation

--- src/components/nav/Navigation.jsx
+++ src/components/nav/Navigation.jsx
@@ -1,17 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
+import "./navigation.css";
 import {AiOutlineHome, AiOutlineUser} from "react-icons/ai";
 import {BiBook} from "react-icons/bi";
 import {RiServiceLine} from "react-icons/ri";
 import {TiContacts} from "react-icons/ti";
 
 function Navigation(props) {
+    const [active, setActive] = useState("#");
+
     return (
         <nav>
-            <a href="#"><AiOutlineHome/></a>
-            <a href="#about"><AiOutlineUser/></a>
-            <a href="#experience"><BiBook/></a>
-            <a href="#services"><RiServiceLine/></a>
-            <a href="#contacts"><TiContacts/></a>
+            <a href="#"
+               onClick={() => setActive("#")}
+               className={active === "#" ? "active" : ""}
+            >
+                <AiOutlineHome/>
+            </a>
+            <a href="#about"
+               onClick={() => setActive("#about")}
+               className={active === "#about" ? "active" : ""}
+            >
+                <AiOutlineUser/>
+            </a>
+            <a href="#experience"
+               onClick={() => setActive("#experience")}
+               className={active === "#experience" ? "active" : ""}
+            >
+                <BiBook/>
+            </a>
+            <a href="#services"
+               onClick={() => setActive("#services")}
+               className={active === "#services" ? "active" : ""}
+            >
+                <RiServiceLine/>
+            </a>
+            <a href="#contacts"
+               onClick={() => setActive("#contacts")}
+               className={active === "#contacts" ? "active" : ""}
+            >
+                <TiContacts/>
+            </a>
         </nav>
     );
 }
