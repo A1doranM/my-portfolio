bdef7b3afd0531c246ac424f4bac8125be997bf5
A1doranM <vaselisk1997@gmail.com>
2022-04-11 13:46:12 +0300

    completed header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./header.css";
 import CTA from "./CTA";
 import ME from "../../assets/me.png";
+import HeaderSocials from "./HeaderSocials";
 
 function Header(props) {
     return (
@@ -10,13 +11,16 @@ function Header(props) {
                 <h5>Hello I`m</h5>
                 <h1>Maksym Medvediev</h1>
                 <h5 className="text-light">Fullstack Developer</h5>
+
                 <CTA/>
+
+                <HeaderSocials/>
 
                 <div className="me">
                     <img src={ME} alt="Image of me."/>
                 </div>
 
-                <a href="#contacts" className="scroll_down">Scroll Down</a>
+                <a href="#contacts" className="scroll__down">Scroll Down</a>
             </div>
         </header>
     );
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import {BsLinkedin, BsGithub, BsDribbble} from "react-icons/bs";
 
 function HeaderSocials(props) {
     return (
         <div className="header__socials">
-            <a href="https://linkedin.com" target="_blank"></a>
-            <a href="https://linkedin.com" target="_blank"></a>
+            <a href="www.linkedin.com/in/maksym-medvediev-93b208132" target="_blank"><BsLinkedin/></a>
+            <a href="https://github.com" target="_blank"><BsGithub/></a>
+            <a href="https://dribble.com" target="_blank"><BsDribbble/></a>
         </div>
     );
 }
